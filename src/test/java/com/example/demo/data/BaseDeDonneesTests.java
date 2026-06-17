d4cb59f49e8f35f14e6fc80dbca4f8c03cfda7eb
rayanbelq <rayan.belqasmi@hotmail.com>
2026-06-17 18:39:26 +0200

    tp4 : correction ajout de l'import manquant pour DemoApplication

--- src/test/java/com/example/demo/data/BaseDeDonneesTests.java
+++ src/test/java/com/example/demo/data/BaseDeDonneesTests.java
@@ -1,14 +1,8 @@
 package com.example.demo.data;
 
+import com.example.demo.DemoApplication;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.Collections;
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class BaseDeDonneesTests {
